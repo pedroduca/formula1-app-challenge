7f9084fb73398242e81d1523f4a012f78ee344c4
Pedro Duca <testandoduca@gmail.com>
2024-02-16 22:45:01 -0300

    feat: criado tela racing

---
+++ src/screens/Racing/Racing.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native'
+
+import Container from '../../components/Container'
+import Tab from '../../components/Tab'
+
+const Racing = () => {
+  return (
+    <>
+      <Container>
+        <Header>
+          <Title maxFontSizeMultiplier={1.1}>Racing</Title>
+
+          <Tab textTabLeft='Upcoming' textTabRight='Past' />
+        </Header>
+      </Container>
+    </>
+  )
+}
+
+const Header = styled.View`
+  margin-top: 24px;
+  margin-left: 27px;
+`
+
+const Title = styled.Text`
+  font-family: 'Poppins_700Bold';
+  font-size: 34px;
+  color: ${({ theme }) => theme.color.white};
+`
+
+export default Racing
