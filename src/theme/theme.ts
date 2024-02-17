41d90a5525580a5605c64ec6ed21805bf279d8c5
Pedro Duca <testandoduca@gmail.com>
2024-02-16 22:43:39 -0300

    feat: add theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    white: '#fff',
+
+    gray: '#D9D9D9',
+    gray25: '#DDDDDD',
+
+    black: '#11100F',
+
+    red: '#EE0000',
+  },
+
+  family: {
+    regular: 'Poppins_400Regular',
+    bold: 'Poppins_700Bold',
+  },
+}
