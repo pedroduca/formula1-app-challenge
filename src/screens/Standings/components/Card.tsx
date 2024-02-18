013272653c7ca3f4bc13bf76233767f7e535b27a
Pedro Duca <testandoduca@gmail.com>
2024-02-18 11:05:42 -0300

    feat: create card component

---
+++ src/screens/Standings/components/Card.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+import { View, StyleSheet, Image } from 'react-native'
+import styled from 'styled-components/native'
+import { SvgProps } from 'react-native-svg'
+
+interface ICardProps {
+  number: number
+  title: string
+  subtitle: string
+  description: string
+  imageCaption: string
+  image: JSX.Element
+}
+
+const Card = ({
+  number,
+  title,
+  subtitle,
+  description,
+  imageCaption,
+  image,
+}: ICardProps) => {
+  return (
+    <CardContainer>
+      <View style={styles.cardLeftSide}>
+        <H1>{number}</H1>
+
+        <View style={styles.gapName}>
+          <View>
+            <H2>{title}</H2>
+            <H1>{subtitle}</H1>
+          </View>
+
+          <Subtitle>{description}</Subtitle>
+        </View>
+      </View>
+
+      <View style={styles.cardRightSide}>
+        {image}
+        <View style={styles.imageSubtitleContainer}>
+          <ImageCaption>{imageCaption}</ImageCaption>
+        </View>
+      </View>
+    </CardContainer>
+  )
+}
+
+const CardContainer = styled.View`
+  flex: 1;
+  background-color: #1b1a19;
+  border-radius: 20px;
+  flex-direction: row;
+
+  padding-top: 20px;
+  padding-left: 20px;
+  padding-bottom: 20px;
+  padding-right: 8px;
+`
+
+const H1 = styled.Text`
+  color: #fff;
+  font-size: 20;
+  font-family: Poppins_700Bold;
+  line-height: 25;
+  letter-spacing: 0.34;
+`
+
+const H2 = styled.Text`
+  color: #fff;
+  font-size: 16;
+  font-family: Poppins_700Bold;
+  line-height: 21;
+`
+
+const Subtitle = styled.Text`
+  color: #cccdd7;
+  font-size: 11;
+  font-family: Poppins_400Regular;
+  line-height: 13;
+`
+
+const ImageCaption = styled(H1)`
+  text-align: center;
+`
+
+const styles = StyleSheet.create({
+  cardLeftSide: {
+    flex: 1,
+    gap: 16,
+    alignContent: 'center',
+  },
+
+  cardRightSide: {
+    alignItems: 'center',
+  },
+  gapName: {
+    gap: 8,
+  },
+  imageSubtitleContainer: {
+    marginTop: -14,
+  },
+})
+
+export default Card
