c72f35d2bd2d71422bcc49b741c1bab223c20e46
Pedro Duca <testandoduca@gmail.com>
2024-02-18 11:06:21 -0300

    feat: add props to card component

--- src/screens/Standings/Standings.tsx
+++ src/screens/Standings/Standings.tsx
@@ -2,9 +2,29 @@ import styled from 'styled-components/native'
 
 import Container from '../../components/Container'
 import Tab from '../../components/Tab'
-import { View } from 'react-native'
+import Card from './components/Card'
+
+import PilotoSVG from '../../assets/images/max-verstappen.svg'
 
 const Standings = () => {
+  const pilotInfoMock = {
+    position: 1,
+    name: 'Max',
+    lastName: 'Verstappen',
+    team: 'Red Bull Racing',
+    points: '255 PTS',
+    image: <PilotoSVG />,
+  }
+
+  const cardMock = {
+    number: pilotInfoMock.position,
+    title: pilotInfoMock.name,
+    subtitle: pilotInfoMock.lastName,
+    description: pilotInfoMock.team,
+    imageCaption: pilotInfoMock.points,
+    image: pilotInfoMock.image,
+  }
+
   return (
     <>
       <Container>
@@ -12,14 +32,24 @@ const Standings = () => {
           <Title maxFontSizeMultiplier={1.1}>Standings</Title>
           <Tab textTabLeft='Drivers' textTabRight='Constructors' />
         </Header>
+
+        <CardList>
+          <Card {...cardMock} />
+        </CardList>
       </Container>
     </>
   )
 }
 
+const CardList = styled.View`
+  margin-horizontal: 27px;
+  gap: 15px;
+`
+
 const Header = styled.View`
   margin-top: 24px;
   margin-left: 27px;
+  margin-bottom: 24px;
   gap: 32px;
 `
 
